621f0b717c162a0deaaac50bc582c82a6d8eeee1
Karunagaran Ulaganathan <karunagu@amazon.com>
2019-03-07 23:25:19 -0800

    multiplication passed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-console.log(testSum(4, 7));
+// console.log(testSum(4, 7));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var multiply_return =[];
+  multiply_return[0] = a*b;
+  multiply_return[1] = 'The product of '+ a + ' and ' + b +' is ' + multiply_return[0] +'.';
+  return multiply_return;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
